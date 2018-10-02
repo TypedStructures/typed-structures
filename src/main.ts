c26c8ef46fa2def2ccef5ff25dda3052ced2ec37
Anthony Pierrat <anthony.pierrat@outlook.fr>
2018-10-02 19:58:10 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { SinglyLinkedList } from './linked-list/singly-linked-list';
+
+let list = new SinglyLinkedList<number>();
+list.push(5);
+list.push(6);
+// list.forEach( (item: number, index: number, array: SinglyLinkedList<number>) => console.log(item * 2, index, array));
+// console.log(list.filter( (item: number, index: number, array: SinglyLinkedList<number>) => item === 5).length());
+console.log(list.indexOf(7));
