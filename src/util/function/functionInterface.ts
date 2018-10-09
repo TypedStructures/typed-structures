41a0ffaa477853dc7c7717e7a5a69ac14ea7fc75
Thibaud REY <contact@thibaudrey.pro>
2018-10-10 01:07:34 +0200

    Rename interfaces

--- src/util/function/functionInterface.ts
+++ src/util/function/functionInterface.ts
@@ -1,4 +1,4 @@
-export interface Function<T, R> extends FunctionConstructor {
+export interface FunctionInterface<T, R> extends FunctionConstructor {
 
     /**
      * Returns a composed function that first applies this function to its input, and then applies the after function to the result.
@@ -7,7 +7,7 @@ export interface Function<T, R> extends FunctionConstructor {
      * @param {Function<R, V>} after the function to apply after this function is applied
      * @return {Function<T, R>} a composed function that first applies this function and then applies the after function
      */
-    andThen<V>(after: Function<R, V>): Function<T, R>;
+    andThen<V>(after: FunctionInterface<R, V>): FunctionInterface<T, R>;
 
     /**
      * Applies this function to the given arguments.
@@ -23,12 +23,12 @@ export interface Function<T, R> extends FunctionConstructor {
      * @param {Function<V, T>} before the function to apply before this function is applied
      * @return {Function<V, R>} a composed function that first applies the before function and then applies this function
      */
-    compose<V>(before: Function<V, T>): Function<V, R>;
+    compose<V>(before: FunctionInterface<V, T>): FunctionInterface<V, R>;
 
     /**
      * Returns a function that always returns its input argument.
      * @type T the type of the input and output objects to the function
      * @return {Function<T, T>} a function that always returns its input argument
      */
-    identity<T>(): Function<T, T>;
+    identity<T>(): FunctionInterface<T, T>;
 }
