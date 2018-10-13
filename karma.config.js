d92b604194a9a0dd3f5a8f1eb8770e783a0af634
Thibaud REY <contact@thibaudrey.pro>
2018-10-13 20:14:50 +0200

    Set tests

--- karma.config.js
+++ karma.config.js
@@ -2,35 +2,49 @@ const webpackConfig = require('./webpack.config.js');
 webpackConfig.mode = 'production';
 
 module.exports = function(config) {
-  config.set({
-    singleRun: true,
-    
-    browsers: [
-      'PhantomJS'
-    ],
+    config.set({
+        basePath: '.',
+        frameworks: ['jasmine', 'karma-typescript'],
+        files: [
+            { pattern: 'src/**/*.+(ts|tsx)' },
+            { pattern: 'src/**/*.ts' },
+        ],
+        exclude: [
+            'node_modules/'
+        ],
+        preprocessors: {
+            '**/*.+(ts|tsx)': ['karma-typescript'],
+        },
+        reporters: ['progress'],
 
-    frameworks: [
-      'jasmine'
-    ],
+        karmaTypescriptConfig: {
+            bundlerOptions: {
+                entrypoints: /\.spec\.ts$/,
+            },
+            compilerOptions: {
+                jsx: 'react',
+                module: 'commonjs',
+                sourceMap: true,
+                target: 'ES5',
+                lib: ['DOM', 'ES2016'],
+            },
+            include: [
+                'src/**/*.ts',
+            ],
+        },
+        junitReporter: {
+            outputDir: './',
+            outputFile: 'test-results.xml',
+        },
 
-    files: [
-      'spec.bundle.js'
-    ],
 
-    preprocessors: {
-      'spec.bundle.js': ['webpack']
-    },
+        port: 9876,
+        colors: true,
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false,
 
-    webpack: webpackConfig,
-
-    webpackMiddleware: {
-      stats: 'errors-only'
-    },
-
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
-      require('karma-webpack')
-    ]
-  });
+        browsers: ['ChromeHeadless']
+    });
 };
