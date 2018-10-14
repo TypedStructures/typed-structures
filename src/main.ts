3529d2b7887a79f6c8910899f8b127e732f0bd5d
AnthonyPierrat <apierrat24@gmail.com>
2018-10-15 00:29:05 +0200

    wip implementation + test + fix #1

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import {DoublyLinkedList} from './collections/linked-list/doubly-linked-list/doubly-linked-list';
+
+let list = new DoublyLinkedList<number>();
+list.push(1);
+list.push(2);
+console.log(list);
