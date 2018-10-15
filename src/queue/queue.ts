04e4d5130d0929713d2276e0085abc5e16755af3
Anthony Pierrat <anthony.pierrat@outlook.fr>
2018-10-16 00:09:10 +0200

    documentation simple queue [to be improved]

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -8,14 +8,26 @@ export class Queue<T> {
         this._items = new DoublyLinkedList<T>();
     }
 
+    /**
+     * Add an item to the end of a Queue
+     * @param item item to enqueue
+     */
     public enqueue(item: T): void {
         this._items.push(item);
     }
 
+    /**
+     * Remove an item from the Queue
+     * @returns{T} item dequeued
+     */
     public dequeue(): T {
         return this._items.shift();
     }
 
+    /**
+     * Return the item at the beginning of the Queue without removing it
+     * @returns{T} the item at the beginning
+     */
     public peek(): T {
         return this._items.peek();
     }
