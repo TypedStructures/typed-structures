9427a5ee5574bcbe5bcf20e931a5f4714e9e4a87
AnthonyPierrat <anthony.pierrat@outlook.fr>
2018-10-21 15:15:13 +0200

    removed main code

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import {DoublyLinkedList} from './collections/linked-list/doubly-linked-list/doubly-linked-list';
-
-let list = new DoublyLinkedList<number>();
-list.push(1);
-list.push(2);
-console.log(list);
