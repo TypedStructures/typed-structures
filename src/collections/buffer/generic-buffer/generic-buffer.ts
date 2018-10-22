2e5aeaa1a71df0a98b829ede188943e760ead205
Thibaud REY <contact@thibaudrey.pro>
2018-10-22 19:23:23 +0200

    GenericBuffer and GenericRingBuffer

---
+++ src/collections/buffer/generic-buffer/generic-buffer.ts
@@ -0,0 +1,31 @@
+import { Buffer } from '../buffer';
+import { IBuffer } from '../buffer-interface';
+
+export class GenericBuffer<T> extends Buffer {
+
+    private readonly _buf: T[];
+    private readonly _readonly: boolean;
+
+    constructor(capacity: number, limit: number, position: number, mark: number, readonly: boolean = false) {
+        super(capacity, limit, position, mark);
+        this._buf = new Array<T>(capacity);
+        this._readonly = readonly;
+    }
+
+    public get(): T {
+        this.position(this.position() + 1);
+        return this._buf[this.position() - 1];
+    }
+
+    public put(element: T): IBuffer {
+        if (!this.isReadOnly()) {
+            this._buf[this.position()] = element;
+            this.position(this.position() + 1);
+        }
+        return this;
+    }
+
+    isReadOnly(): boolean {
+        return this._readonly;
+    }
+}
