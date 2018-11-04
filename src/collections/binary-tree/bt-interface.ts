35423b6513bbbc9f79d7729cc741d5b2d6126fce
AnthonyPierrat <apierrat24@gmail.com>
2018-11-04 20:11:26 +0100

    added binary tree interface

---
+++ src/collections/binary-tree/bt-interface.ts
@@ -0,0 +1,26 @@
+export interface IBinaryTree<T> {
+    
+    /**
+     * add an element.
+     * 
+     * @param item the element to add
+     * @returns {boolean} true if added, false otherwise
+     */
+    add(item: T): boolean;
+
+    /**
+     * delete an element.
+     * 
+     * @param item the element to remove
+     * @returns {boolean} true if added, false otherwise
+     */
+    remove(item: T): boolean;
+
+    /**
+     * the first element in the tree.
+     * 
+     * @param item the element to find
+     * @returns {T} the first element
+     */
+    remove(item: T): T;
+}
